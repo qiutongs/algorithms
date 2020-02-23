a2f0bb5fbbe166cdb621cd52bcb01f6741a634b9
qiutongs <songqt01@gmail,com>
2020-02-23 01:16:15 -0800

    minor update

--- leetcode/400_499/425__Word_Squares.java
+++ leetcode/400_499/425__Word_Squares.java
@@ -1,4 +1,3 @@
-// DFS + Trie
 class Solution {
     public List<List<String>> wordSquares(String[] words) {
         TrieNode root = new TrieNode();
@@ -7,11 +6,11 @@ class Solution {
         }
         
         List<List<String>> ret = new ArrayList<>();
-        dfs(words, new boolean[words.length], root, new ArrayList<>(), ret);
+        dfs(words, root, new ArrayList<>(), ret);
         return ret;
     }
     
-    private void dfs(String[] words, boolean[] visited, TrieNode root, List<Integer> curList, List<List<String>> ret) {
+    private void dfs(String[] words, TrieNode root, List<Integer> curList, List<List<String>> ret) {
         if (curList.size() == words[0].length()) {
             List<String> ans = new ArrayList<>(curList.size());
             for (Integer index : curList) {
@@ -20,22 +19,10 @@ class Solution {
             ret.add(ans);
             return;
         }
-        if (curList.isEmpty()) {
-            for (int i = 0; i < words.length; i++) {
-                visited[i] = true;
-                curList.add(i);
-                dfs(words, visited, root, curList, ret);
-                curList.remove(curList.size() - 1);
-                visited[i] = false;
-            }
-        } else {
-            for (Integer index : search(words, root, curList)) {
-                visited[index] = true;
-                curList.add(index);
-                dfs(words, visited, root, curList, ret);
-                curList.remove(curList.size() - 1);
-                visited[index] = false;
-            }
+        for (Integer index : search(words, root, curList)) {
+            curList.add(index);
+            dfs(words, root, curList, ret);
+            curList.remove(curList.size() - 1);
         }
     }
     
